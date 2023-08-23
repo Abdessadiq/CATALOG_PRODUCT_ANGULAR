35d25c76cc29f64bce68cc30a711b7e3806f5685
Abdessadiq <abdoubhm2018@gmail.com>
2023-08-23 23:38:07 +0100

    Ajouter le traitement pour afficher les products..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./products/products.component";
+import {CustomersComponent} from "./customers/customers.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "products", component: ProductsComponent},
+  {path : "customers", component : CustomersComponent},
+];
 
 @NgModule({
+  // fait appel au tables routes déclarer en haut..
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
